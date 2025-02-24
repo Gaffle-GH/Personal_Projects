ea4ef11aa66280247f6b35f30b60003ad60be176
Gaffle-GH <vinhjrson@gmail.com>
2025-02-24 00:19:36 -0500

    C#

---
+++ c#/testrun/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace testrun
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, this is a C# Test Run!");
+        }
+    }
+}
