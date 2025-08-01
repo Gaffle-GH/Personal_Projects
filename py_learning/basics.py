00aedadb2a8a7bc779c1919573e6ee99b091897f
V <vinhjrson@gmail.com>
2025-08-01 19:28:55 -0400

    Add files via upload

---
+++ py_learning/basics.py
@@ -0,0 +1,49 @@
+# Sys Version
+import sys
+print(sys.version)
+
+# Print
+print("Hello, World!")
+
+# Py Indentation
+if 5 > 2:
+    print("Five is greater than two!")
+
+# Py Indentation (Wrong) 
+#if 5 > 2:
+#print("Five is greater than two!")  # This will cause an Indentation Error
+
+# or
+
+#if 5 > 2:
+#    print("Five is greater than two!")
+        #print("Five is greater than two!")  # This will cause an Indentation Error
+
+# Variables (Variables are created when you assign a value to it)
+x = 5 
+y = "Hello, 'y' Variable!"
+print(x)
+print(y)
+
+# Variable Type Casting
+x = str(10) # strings '10'
+y = int(10) # integers 10
+z = float(10) # float 10.0
+print(x)
+print(y)
+print(z)
+
+# Printing the Type of Variables
+print(type(x))
+print(type(y))
+print(type(z))
+
+# Single and Double Quotes & Case Sensitivity
+a = 5
+A = "Hello, 'A' Variable!" #A will not overwrite a
+print(a)
+print(A)
+
+# Ending
+exit()
+
